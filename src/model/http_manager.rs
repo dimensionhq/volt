b5651e48e52f5d156887d04ff00f6175ae8381f3
XtremeDevX <xtremedevx@gmail.com>
2021-05-13 08:29:17 +0400

    feat: dependency handling

--- src/model/http_manager.rs
+++ src/model/http_manager.rs
@@ -3,7 +3,7 @@ use reqwest::Error;
 use crate::classes::package::Package;
 
 pub async fn get_package(name: &str) -> Result<Package, Error> {
-    reqwest::get(format!("https://registry.yarnpkg.com/{}", name))
+    reqwest::get(format!("http://registry.yarnpkg.com/{}", name))
         .await?
         .json()
         .await
