dc6804e40973ebe385facc6e3beb979507759ef9
Ari Seyhun <ariseyhun@live.com.au>
2021-05-13 18:52:58 +0930

    refactor: move ownership to commands

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -34,5 +34,5 @@ Options:
         )
     }
 
-    async fn exec(&self, _app: Arc<App>, _packages: &Vec<String>, _flags: &Vec<String>) {}
+    async fn exec(&self, _app: Arc<App>, _packages: Vec<String>, _flags: Vec<String>) {}
 }
