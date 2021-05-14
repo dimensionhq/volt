a8df44c95a30bdb312cca1c21e751d67c059eab3
XtremeDevX <xtremedevx@gmail.com>
2021-05-14 06:52:40 +0400

    feat: apply license to files

--- src/model/http_manager.rs
+++ src/model/http_manager.rs
@@ -1,3 +1,19 @@
+/*
+    Copyright 2021 Volt Contributors
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 use std::io;
 
 use thiserror::Error;
@@ -15,6 +31,9 @@ pub enum GetPackageError {
 }
 
 pub async fn get_package(name: &str) -> Result<Option<Package>, GetPackageError> {
+    /*
+
+    */
     let resp = chttp::get_async(format!("http://registry.yarnpkg.com/{}", name))
         .await
         .map_err(GetPackageError::Request)?;
