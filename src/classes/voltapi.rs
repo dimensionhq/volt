655769e34b7c38c4e430d20567f484cbfaa2bcd3
XtremeDevX <xtremedevx@gmail.com>
2021-05-18 21:42:58 +0400

    feat: edit json files and start work on prototype api's

--- src/classes/voltapi.rs
+++ src/classes/voltapi.rs
@@ -4,19 +4,20 @@ use std::collections::HashMap;
 #[derive(Deserialize, Serialize, Debug)]
 pub struct VoltResponse {
     pub version: String,
-    #[serde(flatten)]  
+    #[serde(flatten)]
     pub versions: HashMap<String, VersionData>,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct VersionData {
-    pub packages: HashMap<String, Package>,
+    pub packages: HashMap<String, VoltPackage>,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
-pub struct Package {    
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct VoltPackage {
+    pub name: String,
     pub version: String,
     pub tarball: String,
     pub sha1: String,
-    pub bin: Option<HashMap<String, String>>
-}   
+    pub bin: Option<HashMap<String, String>>,
+}
