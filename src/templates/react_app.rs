28e3e1b00d2f5d4a2dd5b6e0f8b267856a513810
Shadow7258 <pranav.rdoshi@gmail.com>
2021-05-20 13:21:05 +0530

    fix: added dirs in create

--- src/templates/react_app.rs
+++ src/templates/react_app.rs
@@ -3,5 +3,9 @@ use colored::Colorize;
 
 pub fn create_react_app(app_name: String) {
     println!("creating react app: {}", app_name.bright_green());
-    fs::create_dir(app_name).unwrap_or_else(|e| println!("{} {}", "error".bright_red(), e));
+    fs::create_dir(app_name.clone()).unwrap_or_else(|e| println!("{} {}", "error".bright_red(), e));
+    fs::create_dir(format!("{}/src", app_name)).unwrap_or_else(|e| println!("{} {}", "error".bright_red(), e));
+    fs::create_dir(format!("{}/public", app_name)).unwrap_or_else(|e| println!("{} {}", "error".bright_red(), e));
+    fs::File::create(format!("{}/package.json", app_name)).unwrap();
+    fs::File::create(format!("{}/README.md", app_name)).unwrap();
 }
