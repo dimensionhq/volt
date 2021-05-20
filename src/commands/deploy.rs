da0d989e19e916bab78a047a8ee55b98de6c8352
Shadow7258 <pranav.rdoshi@gmail.com>
2021-05-20 10:52:25 +0530

    feat: deploy command

--- src/commands/deploy.rs
+++ src/commands/deploy.rs
@@ -73,6 +73,8 @@ Options:
         let args: Vec<String> = app.args.clone();
         let commit_msg = &args[0];
         println!("{}", commit_msg);        
+        std::env::set_current_dir(std::env::current_dir()?)?;
+        println!("current dir: {:?}", std::env::current_dir());
         std::process::Command::new("git").args(&["add", "."]).output().expect("Failed to add");
         std::process::Command::new("git").args(&["commit", "-m", commit_msg.as_str()]).output().expect("Failed to commit");
         // std::process::Command::new("git").args(&["push"]).output().expect("Failed to push");
