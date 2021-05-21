d6456aaa8de63a4dc3b03df240adc1d1a0abc909
XtremeDevX <xtremedevx@gmail.com>
2021-05-21 12:24:29 +0400

    fix: automation script

--- api/server/automate.py
+++ api/server/automate.py
@@ -1,175 +1,131 @@
-import re
-import json
 import requests
+import json
+import re
+
+
+def get_dependencies(data, version):
+    try:
+        return list(data['versions'][version]['dependencies'].keys())
+    except KeyError:
+        return []
+
+
+def get_cleaned_version(unclean_version, data):
+    version = ''
+
+    if unclean_version == "*":
+        version = list(data['versions'].keys())[-1]
+
+    if '~' in unclean_version:
+        # install latest patch version
+        versions = list(data['versions'].keys())
+
+        patch_version = '0'
+        for pv in versions:
+            opv = pv
+            pv = pv.split('.')[:2]
+            pv[0] = '~' + pv[0]
+
+            if not re.findall(r'[a-z]', opv) and unclean_version.split('.')[:2] == pv and int(opv.split('.')[-1]) > int(
+                patch_version[0]
+            ):
+                patch_version = opv[-1]
+
+        version = '.'.join(unclean_version.split('.')
+                           [:2]) + '.' + patch_version
+        version = version.replace('~', '')
+
+    if '^' in unclean_version:
+        # latest minor and patch version
+        versions = list(data['versions'].keys())
+        minor_version = '0'
+        patch_version = '0'
+
+        skip = False
+        for v in versions:
+            if not re.findall(r'[a-z]', v) and versions[-1] == unclean_version.replace('^', ''):
+                version = unclean_version.replace('^', '')
+                skip = True
+                break
+
+            if not re.findall(r'[a-z]', v) and v[0] == unclean_version[1]:
+                split = v.split('.')[1]
+                if not re.findall(r'[a-z]', v) and int(split) > int(minor_version):
+                    minor_version = split
+                    patch_version = '0'
+
+                split2 = v.split('.')[2]
+
+                if not re.findall(r'[a-z]', v) and int(split2) > int(patch_version):
+                    patch_version = split2
+
+        if not skip:
+            major_version = unclean_version[1]
+            version = major_version + '.' + minor_version + '.' + patch_version
+
+    version = version.replace('^', '').replace('~', '')
+
+    return version if version != '' else unclean_version
+
+
+def get_dependencies_recursive(generated, dependency, unclean_version, main_version):
+    response = requests.get(
+        rf'http://registry.yarnpkg.com/{dependency}').json()
+
+    version = get_cleaned_version(unclean_version, response)
+
+    dependencies = get_dependencies(response, version)
+
+    generated[main_version]['packages'][dependency] = {
+        'name': dependency,
+        'version': version,
+        'tarball': response['versions'][version]['dist']['tarball'],
+        'sha1': response['versions'][version]['dist']['shasum'],
+        'dependencies': dependencies,
+    }
+
+    if 'bin' in list(response['versions'][version].keys()):
+        generated[main_version]['packages'][package]['bin'] = response['versions'][version]['bin']
+
+    try:
+        for dep, vers in response['versions'][version]['dependencies'].items():
+            get_dependencies_recursive(generated, dep, vers, main_version)
+    except KeyError:
+        pass
+
 
 generated = {}
 
-name = input('Name: ')
-
-response = requests.get(f'http://registry.npmjs.com/{name}').json()
-version = response['dist-tags']['latest']
-parent_version = version
-
-generated['version'] = version
-generated[version] = {}
-generated[parent_version]['packages'] = {name: {}}
-
-generated[parent_version]['packages'][name]['name'] = name
-generated[parent_version]['packages'][name]['version'] = version
-generated[parent_version]['packages'][name]['tarball'] = response['versions'][version]['dist']['tarball']
-generated[parent_version]['packages'][name]['sha1'] = response['versions'][version]['dist']['shasum']
-try:
-    generated[parent_version]['packages'][name]['dependencies'] = list(
-        response['versions'][version]['dependencies'].keys())
-except KeyError:
-    with open(rf'C:\Users\xtrem\Desktop\volt\api\server\public\{name}.json', 'w+') as f:
-        f.write(json.dumps(generated))
-    exit()
+package = input('Enter package name: ')
 
 
-def add_dependencies_recursive(package_name: str, generated: dict, version: str):
-    response = requests.get(f'http://registry.npmjs.com/{package_name}').json()
-    generated[parent_version]['packages'][package_name] = {}
+main_response = requests.get(rf'http://registry.yarnpkg.com/{package}').json()
 
-    if version == "*":
-        version = list(response['versions'].keys())[-1]
+main_version = main_response['dist-tags']['latest']
+main_version = get_cleaned_version(main_version, main_response)
 
-    if '~' in version:
-        # install latest patch version
-        versions = list(response['versions'].keys())
-        patch_version = '0'
-        for pv in versions:
-            opv = pv
-            pv = pv.split('.')[:2]
-            pv[0] = '~' + pv[0]
+# Set Version
 
-            if version.split('.')[:2] == pv and int(opv.split('.')[-1]) > int(
-                patch_version[0]
-            ):
-                patch_version = opv[-1]
-
-        version = '.'.join(version.split('.')[:2]) + '.' + patch_version
-        version = version.replace('~', '')
-
-    if '^' in version:
-        # latest minor and patch version
-        versions = list(response['versions'].keys())
-        minor_version = '0'
-        patch_version = '0'
-
-        skip = False
-        for v in versions:
-            if not re.findall(r'[a-z]', v) and versions[-1] == version.replace('^', ''):
-                version = version.replace('^', '')
-                skip = True
-                break
-
-            if not re.findall(r'[a-z]', v) and v[0] == version[1]:
-                split = v.split('.')[1]
-                if not re.findall(r'[a-z]', v) and int(split) > int(minor_version):
-                    minor_version = split
-                    patch_version = '0'
-
-                split2 = v.split('.')[2]
-
-                if not re.findall(r'[a-z]', v) and int(split2) > int(patch_version):
-                    patch_version = split2
-
-        if not skip:
-            major_version = version[1]
-            version = major_version + '.' + minor_version + '.' + patch_version
-
-    generated[parent_version]['packages'][package_name]['name'] = package_name
-    generated[parent_version]['packages'][package_name]['version'] = version
-    generated[parent_version]['packages'][package_name]['tarball'] = response['versions'][version]['dist']['tarball']
-    generated[parent_version]['packages'][package_name]['sha1'] = response['versions'][version]['dist']['shasum']
-    generated[parent_version]['packages'][package_name]['dependencies'] = list(
-        response['versions'][version]['dependencies'].keys())
-
-    if 'bin' in list(response['versions'][version].keys()):
-        generated[parent_version]['packages'][package_name]['bin'] = response['versions'][version]['bin']
-
-    try:
-        for dependency, v in response['versions'][version]['dependencies'].items():
-            v = v.replace('~', '').replace('^', '')
-            add_dependencies_recursive(dependency, generated, v)
-    except KeyError:
-        pass
+generated['version'] = main_version
 
 
-for dependency, version in response['versions'][version]['dependencies'].items():
+generated[main_version] = {'packages': {}}
 
-    response = requests.get(f'http://registry.npmjs.com/{dependency}').json()
-    generated[parent_version]['packages'][dependency] = {}
+generated[main_version]['packages'][package] = {
+    'name': package,
+    'version': main_version,
+    'tarball': main_response['versions'][main_version]['dist']['tarball'],
+    'sha1': main_response['versions'][main_version]['dist']['shasum'],
+    'dependencies': get_dependencies(main_response, main_version)
+}
 
-    if version == "*":
-        version = list(response['versions'].keys())[-1]
+if 'bin' in list(main_response['versions'][main_version].keys()):
+    generated[main_version]['packages'][package]['bin'] = main_response['versions'][main_version]['bin']
 
-    if '~' in version:
-        # install latest patch version
-        versions = list(response['versions'].keys())
-        patch_version = '0'
-        for pv in versions:
-            opv = pv
-            pv = pv.split('.')[:2]
-            pv[0] = '~' + pv[0]
+for main_dependency, version in main_response['versions'][main_version]['dependencies'].items():
+    get_dependencies_recursive(
+        generated, main_dependency, version, main_version)
+    print('Added: ', main_dependency)
 
-            if version.split('.')[:2] == pv and int(opv.split('.')[-1]) > int(
-                patch_version[0]
-            ):
-                patch_version = opv[-1]
-
-        version = '.'.join(version.split('.')[:2]) + '.' + patch_version
-        version = version.replace('~', '')
-
-    if '^' in version:
-        # latest minor and patch version
-        versions = list(response['versions'].keys())
-        minor_version = '0'
-        patch_version = '0'
-
-        skip = False
-        for v in versions:
-            if not re.findall(r'[a-z]', v) and versions[-1] == version.replace('^', ''):
-                version = version.replace('^', '')
-                skip = True
-                break
-
-            if not re.findall(r'[a-z]', v) and v[0] == version[1]:
-                split = v.split('.')[1]
-                if not re.findall(r'[a-z]', v) and int(split) > int(minor_version):
-                    minor_version = split
-                    patch_version = '0'
-
-                split2 = v.split('.')[2]
-
-                if not re.findall(r'[a-z]', v) and int(split2) > int(patch_version):
-                    patch_version = split2
-
-        if not skip:
-            major_version = version[1]
-            version = major_version + '.' + minor_version + '.' + patch_version
-
-    generated[parent_version]['packages'][dependency]['name'] = dependency
-    generated[parent_version]['packages'][dependency]['version'] = version
-    generated[parent_version]['packages'][dependency]['tarball'] = response['versions'][version]['dist']['tarball']
-    generated[parent_version]['packages'][dependency]['sha1'] = response['versions'][version]['dist']['shasum']
-
-    try:
-        generated[parent_version]['packages'][dependency]['dependencies'] = list(
-            response['versions'][version]['dependencies'].keys())
-    except:
-        generated[parent_version]['packages'][dependency]['dependencies'] = []
-
-    if 'bin' in list(response['versions'][version].keys()):
-        generated[parent_version]['packages'][dependency]['bin'] = response['versions'][version]['bin']
-
-    try:
-        for dependency, v in response['versions'][version]['dependencies'].items():
-            v = v.replace('~', '').replace('^', '')
-            add_dependencies_recursive(dependency, generated, v)
-    except KeyError:
-        pass
-
-with open(rf'C:\Users\xtrem\Desktop\volt\api\server\public\{name}.json', 'w+') as f:
+with open(rf'public\{package}.json', 'w+') as f:
     f.write(json.dumps(generated))
