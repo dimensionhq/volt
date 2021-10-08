40a80a3009faa52295837bc27bb67aa7b5051559
Andrew Gucci <athoma67@asu.edu>
2021-10-08 09:51:43 -0700

    fix(build): builds on linux, mac, and windows

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 fn main() {
-    println!("cargo:rustc-link-lib=nghttp2");
-    println!("cargo:rustc-link-lib=libssl");
-    println!("cargo:rustc-link-lib=libcrypto");
+    if cfg!(windows) {
+        println!("cargo:rustc-link-lib=nghttp2");
+        println!("cargo:rustc-link-lib=libssl");
+        println!("cargo:rustc-link-lib=libcrypto");
+    } else {
+        println!("cargo:rustc-link-lib=nghttp2");
+        println!("cargo:rustc-link-lib=ssl");
+        println!("cargo:rustc-link-lib=crypto");
+    };
 }
