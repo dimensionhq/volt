640f90d59435d23cac42a818d50842965ae5e58a
brokenbyte <git@brokenbyte.io>
2021-10-26 14:01:55 -0700

    wip(node): download nodejs versions to tmp dir

--- src/commands/node.rs
+++ src/commands/node.rs
@@ -12,114 +12,89 @@ limitations under the License.
 */
 
 //! Manage local node versions
-
-use crate::{
+/*use crate::{
     core::{command::Command, VERSION},
     App,
-};
+};*/
 
-use std::io::Cursor;
+use std::str;
+use std::{fmt::Display, fs::File, io::Write};
 
-use std::sync::Arc;
-
-use async_trait::async_trait;
 use clap::ArgMatches;
-use colored::Colorize;
 use miette::Result;
-use node_semver::Range;
-use reqwest;
-use serde::de;
-use serde::Deserialize;
+use node_semver::{Range, Version};
+use serde::{Deserialize, Deserializer};
+use tempfile::tempdir;
+//use async_trait::async_trait;
+//use colored::Colorize;
 
-/*
- * mod lts_status {
- *     use serde::{Deserialize, Deserializer};
- *
- *     #[derive(Deserialize, Debug, PartialEq, Eq)]
- *     #[serde(untagged)]
- *     enum LtsStatus {
- *         Nope(bool),
- *         Yes(String),
- *     }
- *
- *     impl Into<Option<String>> for LtsStatus {
- *         fn into(self) -> Option<String> {
- *             match self {
- *                 Self::Nope(_) => None,
- *                 Self::Yes(x) => Some(x),
- *             }
- *         }
- *     }
- *
- *     pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
- *     where
- *         D: Deserializer<'de>,
- *     {
- *         Ok(LtsStatus::deserialize(deserializer)?.into())
- *     }
- * }
- */
+#[derive(Deserialize)]
+#[serde(untagged)]
+enum Lts {
+    No(bool),
+    Yes(String),
+}
 
-struct LtsVisitor;
-
-/*
- * #[derive(Deserialize, Debug)]
- * struct MyJson {
- *     name: String,
- *     #[serde(deserialize_with = "from_timestamp")]
- *     timestamp: NaiveDateTime,
- * }
- */
-
-use std::fmt;
-
-impl<'de> de::Visitor<'de> for LtsVisitor {
-    type Value = Option<String>;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter, "a string represents chrono::NaiveDateTime")
-    }
-
-    fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
-    where
-        E: de::Error,
-    {
-        /*
-         * if let t = String::from(s) {
-         *     Ok(Some(t))
-         * } else {
-         *     Err(de::Error::invalid_value(de::Unexpected::Str(s), &self))
-         * }
-         */
-
-        /*
-         * match String::from(s) {
-         *     Ok(t) => Ok(Some(t)),
-         *     Err(_) => Err(de::Error::invalid_value(de::Unexpected::Str(s), &self)),
-         * }
-         */
+impl Into<Option<String>> for Lts {
+    fn into(self) -> Option<String> {
+        match self {
+            Self::No(_) => None,
+            Self::Yes(x) => Some(x),
+        }
     }
 }
 
-fn from_timestamp<'de, D>(d: D) -> Result<Option<String>, D::Error>
+pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
 where
-    D: de::Deserializer<'de>,
+    D: Deserializer<'de>,
 {
-    d.deserialize_str(LtsVisitor)
+    Ok(Lts::deserialize(deserializer)?.into())
 }
 
-#[derive(Debug)]
+#[derive(Deserialize, Debug)]
+pub struct NodeVersion {
+    pub version: Version,
+    #[serde(deserialize_with = "deserialize")]
+    pub lts: Option<String>,
+    pub files: Vec<String>,
+}
+
+#[derive(Debug, PartialEq)]
 enum Os {
     Windows,
     Macos,
     Linux,
     Unknown,
 }
-#[derive(Debug)]
+
+impl Display for Os {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let s = match self {
+            &Os::Windows => "win",
+            &Os::Macos => "darwin",
+            &Os::Linux => "linux",
+            _ => unreachable!(),
+        };
+        write!(f, "{}", s)
+    }
+}
+
+#[derive(Debug, PartialEq)]
 enum Arch {
     X86,
     X64,
     Unknown,
+}
+
+impl Display for Arch {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let s = match self {
+            &Arch::X86 => "x86",
+            &Arch::X64 => "x64",
+            _ => unreachable!(),
+        };
+        write!(f, "{}", s)
+    }
 }
 
 fn get_arch() -> Arch {
@@ -130,6 +105,18 @@ fn get_arch() -> Arch {
         Arch::X64
     } else {
         Arch::Unknown
+    }
+}
+
+fn get_platform() -> Os {
+    if cfg!(target_os = "windows") {
+        Os::Windows
+    } else if cfg!(target_os = "macos") {
+        Os::Macos
+    } else if cfg!(target_os = "linux") {
+        Os::Linux
+    } else {
+        Os::Unknown
     }
 }
 
@@ -145,7 +132,6 @@ impl Node {
             }
             Some(("install", versions)) => {
                 let v: Vec<&str> = versions.values_of("versions").unwrap().collect();
-                println!("Installing version {:?}", v);
                 download_node_version(v).await;
             }
             Some(("remove", versions)) => {
@@ -159,76 +145,108 @@ impl Node {
     }
 }
 
-#[derive(Deserialize, Debug)]
-pub struct IndexedNodeVersion {
-    // pub version: Version,
-    pub version: String,
-    #[serde(deserialize_with = "from_timestamp")]
-    pub lts: Option<String>,
-    pub date: chrono::NaiveDate,
-    pub files: Vec<String>,
-}
-
 // 32bit macos/linux systems cannot download a version of node >= 10.0.0
 // They stopped making 32bit builds after that version
+// https://nodejs.org/dist/
+// TODO: Handle errors with file already existing and handle file creation/deletion errors
 async fn download_node_version(versions: Vec<&str>) {
+    // TODO: Only make a tempdir if we have versions to download, i.e. verify all versions before
+    // creating the directory
+    let dir: tempfile::TempDir = tempdir().unwrap();
+    println!("Got tempdir: {}", dir.path().to_str().unwrap());
+
+    let arch = get_arch();
+    let os = get_platform();
+
     let mirror = "https://nodejs.org/dist";
 
-    // let r = reqwest::get().await;
-    // let b = r.unwrap().bytes();
-    // println!("{:?}", b.await);
-    let mut value: Vec<IndexedNodeVersion> = reqwest::get(format!("{}/index.json", mirror))
+    let _node_versions: Vec<NodeVersion> = reqwest::get(format!("{}/index.json", mirror))
         .await
         .unwrap()
         .json()
         .await
         .unwrap();
 
-    let response = reqwest::get("https://nodejs.org/dist/node-v0.1.25.tar.gz");
-
-    let mut file = std::fs::File::create("file.tar.gz").unwrap();
-    let mut content = Cursor::new(response.await.unwrap().bytes().await.unwrap());
-    std::io::copy(&mut content, &mut file).unwrap();
-
-    /*
-     * for v in value {
-     *     println!("{:?}\n", v);
-     * }
-     */
-
-    let os = if cfg!(target_os = "windows") {
-        Os::Windows
-    } else if cfg!(target_os = "macos") {
-        Os::Macos
-    } else if cfg!(target_os = "linux") {
-        Os::Linux
-    } else {
-        Os::Unknown
-    };
-
-    let arch = get_arch();
-
     for v in versions {
-        println!(
-            "Installing version {} for architecture {:?} for OS {:?}",
-            v, arch, os
-        );
+        let mut download_url = format!("{}/", mirror);
+        if let Ok(_) = v.parse::<Version>() {
+            if arch == Arch::X86 && (os == Os::Macos || os == Os::Linux) {
+                let major = v.split('.').next().unwrap().parse::<u8>().unwrap();
+
+                if major >= 10 {
+                    println!("32 bit versions are not available for macos and linux after version 10.0.0!");
+                    return;
+                }
+            }
+
+            let mut found = false;
+            for n in &_node_versions {
+                if v == n.version.to_string() {
+                    // println!("Found matching version: {:?}", n);
+                    download_url = format!("{}v{}", download_url, n.version);
+                    found = true;
+                }
+            }
+
+            if !found {
+                println!("Unable to find version {}!", v);
+                return;
+            }
+
+            if os == Os::Windows {
+                download_url = format!("{}/win-{}/node.exe", download_url, arch);
+            } else {
+                download_url = format!("{}/node-v{}-{}-{}.tar.gz", download_url, v, os, arch);
+            }
+        } else if let Ok(_) = v.parse::<Range>() {
+            //
+            // TODO: Handle ranges with special chars like ^10.3
+            //
+
+            if arch == Arch::X86 && (os == Os::Macos || os == Os::Linux) {
+                let major = v.split('.').next().unwrap();
+                if major.parse::<u8>().unwrap() >= 10 {
+                    println!("32 bit versions are not available for macos and linux after version 10.0.0!");
+                    return;
+                }
+            }
+            todo!("Need to handle ranges");
+        } else {
+            println!("Unable to download {} -- not a valid version!", v);
+            continue;
+        }
+
+        println!("Installing version {}", v);
+        let response = reqwest::get(&download_url).await.unwrap();
+
+        let mut dest = {
+            let fname = response
+                .url()
+                .path_segments()
+                .and_then(|segments| segments.last())
+                .and_then(|name| if name.is_empty() { None } else { Some(name) })
+                .unwrap();
+
+            println!("file to download: '{}'", fname);
+            let fname = dir.path().join(fname);
+            File::create(fname).unwrap()
+        };
+
+        let content = response.bytes().await.unwrap();
+
+        dest.write_all(&content).unwrap();
+        println!("\n---\n");
     }
-
-    let _r: Range = "12".parse().unwrap();
-
-    // println!("In foo func for {:?}!", os);
-    // println!("Version {}", r);
 }
 
-#[async_trait]
+/*#[async_trait]
 impl Command for Node {
     /// Display a help menu for the `volt add` command.
     fn help() -> String {
         format!(
             r#"volt {}
 
-            Add a package to your project's dependencies.
+            Manage NodeJS versions
             Usage: {} {} {} {}
             Options:
 
@@ -252,7 +270,7 @@ impl Command for Node {
         )
     }
 
-    /// Execute the `volt add` command
+    /// Execute the `volt node` command
     ///
     /// Adds a package to dependencies for your project.
     /// ## Arguments
@@ -271,153 +289,8 @@ impl Command for Node {
         let x = x.unwrap();
         for a in x {
             println!("{:?}", a);
+            break;
         }
         Ok(())
-        /*
-         *         // Get input packages
-         *         let mut packages = app.get_packages()?;
-         *
-         *         // Load the existing package.json file
-         *         let (mut package_file, package_file_path) = PackageJson::open("package.json")?;
-         *
-         *         // Construct a path to the local and global lockfile.
-         *         let lockfile_path = &app.lock_file_path;
-         *
-         *         let global_lockfile = &app.home_dir.join(".global.lock");
-         *
-         *         // Load local and global lockfiles.
-         *         let mut lock_file =
-         *             LockFile::load(lockfile_path).unwrap_or_else(|_| LockFile::new(lockfile_path));
-         *
-         *         let mut global_lock_file =
-         *             LockFile::load(global_lockfile).unwrap_or_else(|_| LockFile::new(global_lockfile));
-         *
-         *         // Create progress bar for resolving dependencies.
-         *
-         *         let progress_bar = ProgressBar::new(packages.len() as u64);
-         *
-         *         progress_bar.set_style(
-         *             ProgressStyle::default_bar()
-         *                 .progress_chars(PROGRESS_CHARS)
-         *                 .template(&format!(
-         *                     "{} [{{bar:40.green/magenta}}] {{msg:.blue}}",
-         *                     "Resolving Dependencies".bright_blue()
-         *                 )),
-         *         );
-         *
-         *         // Fetch npm data including hash to fetch dependencies
-         *         let data = npm::get_versions(&packages).await?;
-         *
-         *         // Fetch pre-flattened dependency trees from the registry
-         *         let (responses, elapsed) = fetch_dep_tree(&data, &progress_bar).await?;
-         *
-         *         let mut dependencies: HashMap<String, VoltPackage> = HashMap::new();
-         *
-         *         for res in responses.iter() {
-         *             let current_version = res.versions.get(&res.version).unwrap();
-         *             dependencies.extend(current_version.to_owned());
-         *         }
-         *
-         *         progress_bar.finish_with_message("[OK]".bright_green().to_string());
-         *
-         *         print_elapsed(dependencies.len(), elapsed);
-         *
-         *         let mut dependencies: Vec<_> = dependencies
-         *             .iter()
-         *             .map(|(_name, object)| {
-         *                 let mut lock_dependencies: Vec<String> = vec![];
-         *
-         *                 if let Some(peer_deps) = &object.peer_dependencies {
-         *                     for dep in peer_deps {
-         *                         if !crate::core::utils::check_peer_dependency(&dep) {
-         *                             progress_bar.println(format!(
-         *                                 "{}{} {} has unmet peer dependency {}",
-         *                                 " warn ".black().bright_yellow(),
-         *                                 ":",
-         *                                 object.name.bright_cyan(),
-         *                                 &dep.bright_yellow()
-         *                             ));
-         *                         }
-         *                     }
-         *                 }
-         *
-         *                 if let Some(dependencies) = &object.dependencies {
-         *                     for dep in dependencies {
-         *                         lock_dependencies.push(dep.to_string());
-         *                     }
-         *                 }
-         *
-         *                 let object_instance = object.clone();
-         *
-         *                 lock_file.dependencies.insert(
-         *                     DependencyID(object_instance.name, object_instance.version),
-         *                     DependencyLock {
-         *                         name: object.name.clone(),
-         *                         version: object.version.clone(),
-         *                         tarball: object.tarball.clone(),
-         *                         integrity: object.integrity.clone(),
-         *                         dependencies: lock_dependencies.clone(),
-         *                     },
-         *                 );
-         *
-         *                 let second_instance = object.clone();
-         *
-         *                 global_lock_file.dependencies.insert(
-         *                     DependencyID(second_instance.name, second_instance.version.to_owned()),
-         *                     DependencyLock {
-         *                         name: object.name.clone(),
-         *                         version: object.version.clone(),
-         *                         tarball: object.tarball.clone(),
-         *                         integrity: object.integrity.clone(),
-         *                         dependencies: lock_dependencies,
-         *                     },
-         *                 );
-         *
-         *                 object
-         *             })
-         *             .collect();
-         *
-         *         for dep in dependencies.iter() {
-         *             for package in packages.iter_mut() {
-         *                 if dep.name == package.name {
-         *                     package.version = Some(dep.version.clone());
-         *                 }
-         *             }
-         *         }
-         *
-         *         let progress_bar = ProgressBar::new(dependencies.len() as u64);
-         *
-         *         progress_bar.set_style(
-         *             ProgressStyle::default_bar()
-         *                 .progress_chars(PROGRESS_CHARS)
-         *                 .template(&format!(
-         *                     "{} [{{bar:40.green/magenta}}] {{msg:.blue}}",
-         *                     "Installing Packages".bright_blue()
-         *                 )),
-         *         );
-         *
-         *         dependencies.dedup();
-         *
-         *         dependencies
-         *             .into_iter()
-         *             .map(|v| install_extract_package(&app, &v))
-         *             .collect::<FuturesUnordered<_>>()
-         *             .inspect(|_| progress_bar.inc(1))
-         *             .try_collect::<()>()
-         *             .await
-         *             .unwrap();
-         *
-         *         progress_bar.finish();
-         *
-         *         for package in packages {
-         *             package_file.add_dependency(package);
-         *         }
-         *
-         *         package_file.save()?;
-         *         global_lock_file.save()?;
-         *         lock_file.save()?;
-         *
-         *         Ok(())
-         */
     }
-}
+}*/
