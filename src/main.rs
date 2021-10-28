6f24737b2490804534ea41725eb81c586ed8a5d0
brokenbyte <git@brokenbyte.io>
2021-10-27 22:56:02 -0700

    chore: start implementing logging w/ tracing

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
 /*
-    Copyright 2021 Volt Contributors
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
+ *
+ *    Copyright 2021 Volt Contributors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
 mod commands;
 mod core;
 
-use crate::commands::node::*;
-
-use crate::commands::add::*;
+use crate::commands::{add::*, node::*};
 use crate::core::{command::Command, utils::app::App};
 
 use clap::{Arg, ArgMatches};
 use colored::Colorize;
 use commands::{clean::Clean, clone::Clone, discord::Discord, init::Init};
+use tracing::{self, Level};
+use tracing_subscriber::filter::EnvFilter;
 
+use std::str::FromStr;
 use std::{sync::Arc, time::Instant};
 
 pub async fn map_subcommand(matches: ArgMatches) -> miette::Result<()> {
@@ -57,6 +59,14 @@ pub async fn map_subcommand(matches: ArgMatches) -> miette::Result<()> {
 
 #[tokio::main]
 async fn main() -> miette::Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(Level::TRACE)
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or(EnvFilter::from_str("volt=info").unwrap()),
+        )
+        .without_time()
+        .init();
+
     let start = Instant::now();
     let volt_help = format!(
         r#"{} {}
