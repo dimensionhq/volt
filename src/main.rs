0ab8a3620f7272046afb68869ce338a1ba263e41
brokenbyte <git@brokenbyte.io>
2022-04-05 09:24:37 -0700

    feat(node): progress on threaded downloads

--- src/main.rs
+++ src/main.rs
@@ -25,35 +25,56 @@ mod cli;
 mod commands;
 mod core;
 
-use std::{str::FromStr, time::Instant};
+use std::{io::stdin, str::FromStr, time::Instant};
 
 use tracing::Level;
 use tracing_subscriber::EnvFilter;
 
 use crate::cli::{VoltCli, VoltCommand};
 
-#[tokio::main]
-async fn main() -> miette::Result<()> {
-    tracing_subscriber::fmt()
-        .with_max_level(Level::TRACE)
-        .with_env_filter(
-            EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| EnvFilter::from_str("volt=info").unwrap()),
-        )
-        .without_time()
-        .init();
+//#[tokio::main(worker_threads = 6)]
+//#[tokio::main(flavor = "current_thread")]
+fn main() -> miette::Result<()> {
+    let body = async {
+        tracing_subscriber::fmt()
+            .with_max_level(Level::TRACE)
+            .with_env_filter(
+                EnvFilter::try_from_default_env()
+                    .unwrap_or_else(|_| EnvFilter::from_str("volt=info").unwrap()),
+            )
+            .without_time()
+            .init();
 
-    if cfg!(windows) {
-        core::utils::enable_ansi_support().unwrap();
-    }
+        if cfg!(windows) {
+            core::utils::enable_ansi_support().unwrap();
+        }
 
-    let start = Instant::now();
+        let start = Instant::now();
 
-    let app = VoltCli::new();
+        let app = VoltCli::new();
 
-    app.cmd.exec(app.config).await?;
+        app.cmd.exec(app.config).await?;
 
-    println!("Finished in {:.2}s", start.elapsed().as_secs_f32());
+        println!("Finished in {:.2}s", start.elapsed().as_secs_f32());
 
-    Ok(())
+        Ok(())
+    };
+
+    tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(6)
+        .max_blocking_threads(6)
+        .thread_name("volt")
+        .enable_all()
+        .build()
+        .expect("Failed to build the runtime")
+        .block_on(body)
+
+    /*
+     *tokio::runtime::Builder::new_multi_thread()
+     *    .worker_threads(2)
+     *    .enable_all()
+     *    .build()
+     *    .expect("Failed building the Runtime")
+     *    .block_on(body)
+     */
 }
