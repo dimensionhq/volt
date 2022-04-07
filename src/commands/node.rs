f1e0ca64a00f2c12e43af3dae381a93bf5be32f3
MadPotts <65889270+MadPotts@users.noreply.github.com>
2022-04-06 22:34:18 -0700

    feat(node): initial work on node list

--- src/commands/node.rs
+++ src/commands/node.rs
@@ -147,6 +147,7 @@ impl VoltCommand for Node {
             NodeCommand::Use(x) => x.exec(config).await,
             NodeCommand::Install(x) => x.exec(config).await,
             NodeCommand::Remove(x) => x.exec(config).await,
+            NodeCommand::List(x) => x.exec(config).await,
         }
     }
 }
@@ -156,6 +157,50 @@ pub enum NodeCommand {
     Use(NodeUse),
     Install(NodeInstall),
     Remove(NodeRemove),
+    List(NodeList),
+}
+/// List available NodeJS versions
+#[derive(Debug, Parser)]
+pub struct NodeList {}
+
+#[async_trait]
+impl VoltCommand for NodeList {
+    // On windows, versions install to C:\Users\[name]\AppData\Roaming\volt\node\[version]
+    async fn exec(self, config: VoltConfig) -> Result<()> {
+        let node_path = {
+            let datadir = dirs::data_dir().unwrap().join("volt").join("node");
+            if !datadir.exists() {
+                eprintln!("No NodeJS versions installed!");
+                std::process::exit(1);
+            };
+            datadir
+        };
+
+        let files = std::fs::read_dir(&node_path)
+            .unwrap()
+            .map(|d| {
+                d.unwrap()
+                    .path()
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .to_owned()
+            })
+            .filter(|f| f != "current")
+            .collect::<Vec<String>>();
+
+        if files.len() == 0 {
+            eprintln!("No NodeJS versions installed!");
+            std::process::exit(1);
+        }
+
+        for file in files {
+            println!("{file}");
+        }
+
+        Ok(())
+    }
 }
 
 /// Switch current node version
